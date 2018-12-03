ff003b94ea4c6001586ffdfd9c23d68141cb1013
Luís <luis_m_gouveia@hotmail.com>
2018-12-03 12:25:37 +0000

    Fix hiding extra days in agenda

--- src/agenda/index.js
+++ src/agenda/index.js
@@ -109,6 +109,7 @@ export default class AgendaView extends Component {
       firstResevationLoad: false,
       selectedDay: parseDate(this.props.selected) || XDate(true),
       topDay: parseDate(this.props.selected) || XDate(true),
+      hideExtraDays: false,
     };
     this.currentMonth = this.state.selectedDay.clone();
     this.onLayout = this.onLayout.bind(this);
@@ -131,7 +132,9 @@ export default class AgendaView extends Component {
   }
 
   setScrollPadPosition(y, animated) {
-    this.scrollPad._component.scrollTo({x: 0, y, animated});
+    if (typeof this.scrollPad === 'object') {
+      this.scrollPad._component.scrollTo({ x: 0, y, animated });
+    }
   }
 
   onScrollPadLayout() {
@@ -151,6 +154,7 @@ export default class AgendaView extends Component {
 
   onTouchStart() {
     this.headerState = 'touched';
+    this.setState({ hideExtraDays: true });
     if (this.knob) {
       this.knob.setNativeProps({style: { opacity: 0.5 }});
     }
@@ -184,6 +188,8 @@ export default class AgendaView extends Component {
     this.setScrollPadPosition(snapY, true);
     if (snapY === 0) {
       this.enableCalendarScrolling();
+    } else {
+      this.setState({ hideExtraDays: false });
     }
   }
 
@@ -231,7 +237,8 @@ export default class AgendaView extends Component {
 
   enableCalendarScrolling() {
     this.setState({
-      calendarScrollable: true
+      calendarScrollable: true,
+      hideExtraDays: true,
     });
     if (this.props.onCalendarToggled) {
       this.props.onCalendarToggled(true);
@@ -255,7 +262,8 @@ export default class AgendaView extends Component {
     const day = parseDate(d);
     this.setState({
       calendarScrollable: false,
-      selectedDay: day.clone()
+      selectedDay: day.clone(),
+      hideExtraDays: false,
     });
     if (this.props.onCalendarToggled) {
       this.props.onCalendarToggled(false);
@@ -410,7 +418,7 @@ export default class AgendaView extends Component {
               removeClippedSubviews={this.props.removeClippedSubviews}
               onDayPress={this._chooseDayFromCalendar.bind(this)}
               scrollingEnabled={this.state.calendarScrollable}
-              hideExtraDays={this.state.calendarScrollable}
+              hideExtraDays={this.state.hideExtraDays}
               firstDay={this.props.firstDay}
               monthFormat={this.props.monthFormat}
               pastScrollRange={this.props.pastScrollRange}
